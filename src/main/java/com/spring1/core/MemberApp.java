522ae456b8eeee1f4b2fba0c8f84a5cb0f4718e7
jenniepp <sj323130@nate.com>
2022-09-19 19:50:06 +0900

    0919 Spring Study2

---
+++ src/main/java/com/spring1/core/MemberApp.java
@@ -0,0 +1,21 @@
+package com.spring1.core;
+
+import com.spring1.core.member.Grade;
+import com.spring1.core.member.Member;
+import com.spring1.core.member.MemberService;
+import com.spring1.core.member.MemberServiceImpl;
+
+public class MemberApp {
+
+    //작동을 잘 하는지 간단히 확인하기 위한 TestClass : 순수한 자바코드, 스프링 관련된 코드 0
+    //하지만 이렇게 확인하면 매번 콘솔에 나온 값으로 눈으로 확인해야하고 좋은 테스트가 아니다 > JUnit이용하자
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Member findMember = memberService.findMember(1L);
+        System.out.println("new member = " + member.getName());
+        System.out.println("find Member = " + findMember.getName());
+    }
+}
---
+++ src/test/java/com/spring1/core/member/MemberServiceTest.java
@@ -0,0 +1,4 @@
+package com.spring1.core.member;
+
+public class MemberServiceTest {
+}
